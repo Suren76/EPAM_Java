4d07416023eed6c7907cf3c4dfafb61bc71077c6
Suren <parsyansuren@gmail.com>
2024-09-02 02:13:56 +0400

    initial commit, homework0

---
+++ homework0/PrintTextInOneLine.java
@@ -0,0 +1,7 @@
+class PrintTextInOneLine {
+    public static void main(String[] args) {
+        System.out.print("Downcast Face with Sweat emoji: (~_~;)," + "\t");
+        System.out.print("Joyful emoji: ^_^," + "\t");
+        System.out.print("Terribly sad emoji: (´；ω；`)," + "\t");
+    }
+}
