a6bb5991feb13e9f4855fab42eba828b76ddd551
Suren <parsyansuren@gmail.com>
2024-11-26 03:22:36 +0400

    homework9: OOP Final and Static keywords: add fixes

--- homework9OOPFinalStatic/GameClub.java
+++ homework9OOPFinalStatic/GameClub.java
@@ -2,14 +2,14 @@ package homework9OOPFinalStatic;
 
 public class GameClub {
     public static void main(String[] args) {
-        long first_pc_memory_size = 32L*1024*1024*1024;
-        long first_pc_rom_size = 512L*1024*1024*1024;
+        long firstPcMemorySize = 32L*1024*1024*1024;
+        long firstPcRomSize = 512L*1024*1024*1024;
 
-        Computer firstPc = new Computer(first_pc_memory_size, first_pc_rom_size);
+        Computer firstPc = new Computer(firstPcMemorySize, firstPcRomSize);
 
-        long pc_to_break_limits_memory_size = 128L*1024*1024*1024;
-        long pc_to_break_limits_rom_size = 5L*1024*1024*1024*1024;
+        long pcToBreakLimitsMemorySize = 128L*1024*1024*1024;
+        long pcToBreakLimitsRomSize = 5L*1024*1024*1024*1024;
 
-        Computer pc_to_break_limits = Computer.generateMegaComputer(pc_to_break_limits_memory_size, pc_to_break_limits_rom_size);
+        Computer pc_to_break_limits = Computer.generateMegaComputer(pcToBreakLimitsMemorySize, pcToBreakLimitsRomSize);
     }
 }
